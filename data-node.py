376b7521ad7d0bb38f6dbff943c8556133a65e41
Edgar Montano <em2532@nyu.edu>
2015-12-11 20:13:34 -0500

    adding documentation

--- data-node.py
+++ data-node.py
@@ -11,60 +11,68 @@ import sys
 class DataNode(data_pb2.BetaDataNodeServicer):
     
     def Store(self,request,context):
-        """Stores a file on the data node
+        """Stores a file on the data node, DataNode should make a call
+        to our custom DFSS. Therefore it makes a call to dfss.Store()
+        after the proper parameters are passed.
+
+        StoreRequest takes the following parameters:
+            1. string file_name
+            2. bytes file_content 
+            3. string timestamp
+            4. string user_id
+
+        StoreReply takes the following parameters:
+            1. string reply_msg
+            2. bool success
         """
-        
-        reply_msg = "Error has occured, file has not been written"
-        
+         
         if commonlib.DEBUG:
             print("Attempting to access DataNode.Store") 
+        
+        #test if the request parameters are properly passed, complain if
+        #nonetype
         try:
-            if commonlib.DEBUG:
-                print("Attempting to print file requests in DataNode.Store")
             fn = request.file_name
             fc = request.file_content
             ts = request.timestamp
             uid = request.user_id
-            print("FC= %s ; Timestamp=%s ; uid=%s" % (fn,ts,uid))
         except:
             print("Error in retrieving requests")
             return data_pb2.StoreReply(reply_msg="Err -2",success=False)
         
         #NOTE: Adjust parameters for dfss store operation 
         dfss.Store(fn,fc,ts,uid)
-        
+        reply_msg = "File successfully written"
+
         return data_pb2.StoreReply(reply_msg=request.file_name,success=True)
 
     def Read(self,request,context):
-        """Reads a file from data node
+        """Reads a file from data node. This should simply call
+        dfss.Read() with the proper parameters.
+
+        ReadRequest takes the following parameters:
+            1. string file_name
+            2. string timestamp
+
+        ReadReply takes the following parameters:
+            1. bytes reply_file
+            2. bool success
         """
        
         if commonlib.DEBUG:
             print("attempting to connect to DataNode Read...")
-        filename =  request.file_name
-        block_size = request.block_size 
        
-        #if the block_size flag has not been set, set it to the default
-        #1mb
-        if block_size == 0:
-            block_size = commonlib.MB 
-        
-        fd = commonlib.splitFile(filename,block_size)
-        
-        
-        if commonlib.DEBUG:
-            print("reading file ")
-       
+        #ensure that requests are not nonetype
         try:
-            if commonlib.DEBUG:
-                print("Attempting to create dfss.Read() req")
             fn = request.file_name
             ts = request.timestamp
         except:  
-            print("Requests are empty") 
-        dfss.Read(fn,ts)
-
-        return data_pb2.ReadReply(reply_file=fd[0])
+            print("Requests are empty")
+            
+        #NOTE: Replace with new dfss.Read() 
+        fd = dfss.Read(fn,ts)
+        fd = str(fd)
+        return data_pb2.ReadReply(reply_file=fd,success=True)
     
     def isAlive(self,request,context):
         """This responds with a message indicating the service is alive.
